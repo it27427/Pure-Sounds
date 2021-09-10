ad0afa5057dae817208e9ad3b8a562a8d85c326d
Istiak-Tushar42 <istiak.tushar.qtec@gmail.com>
2021-09-10 18:17:30 +0600

    load more less file added

--- assets/js/multi-range.js
+++ assets/js/multi-range.js
@@ -67,48 +67,6 @@ $inputTo.on("change", function () {
     $(this).prop("value", val);
 });
 
-// SEE MORE AND LESS EVENT
-const seeless = document.querySelector('.btn-showless');
-const showmore = document.querySelector('.btn-showall');
-
-seeless.addEventListener('click', toggleSeeLess);
-showmore.addEventListener('click', toggleMore);
-
-function toggleSeeLess() {
-    const tagbox = document.querySelector('.tag-buttons');
-
-    if(tagbox.classList.contains('show')) {
-        tagbox.classList.remove('show');
-        seeless.textContent = 'See more';
-    } else {
-        tagbox.classList.add('show');
-        seeless.textContent = 'See less';
-    }
-}
-
-function toggleMore() {
-    const libraryBox = document.querySelector('.library-buttons');
-
-    if(libraryBox.classList.contains('show')) {
-        libraryBox.classList.remove('show');
-        showmore.textContent = 'See more';
-    } else {
-        libraryBox.classList.add('show');
-        showmore.textContent = 'See less';
-    }
-}
-
-const collapseBtn = document.querySelector('.btn-collapse');
-const collapse = document.querySelector('.collapse');
-
-
-collapseBtn.addEventListener('click', () => {
-    if(collapseBtn.classList.contains('active')) {
-        collapseBtn.classList.remove('remove');
-    } else {
-        collapseBtn.classList.add('active');
-    }
-});
 
 // $(document).ready(function () {
 //   $('#sidebarMenu').hide();
