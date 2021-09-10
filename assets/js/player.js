001246cf7489ba3ec08a74eff893b6eab89a8a5d
Istiak-Tushar42 <istiak.tushar.qtec@gmail.com>
2021-09-11 00:21:32 +0600

    Filter completed with responsive

--- assets/js/player.js
+++ assets/js/player.js
@@ -1,15 +1,6 @@
-// let playList = [
-//     "assets/audio/track1.mp3",
-//     "assets/audio/track2.mp3",
-//     "assets/audio/track3.mp3",
-//     "assets/audio/track4.wav"
-// ];
-
 let soundtrack = Array.from(document.querySelectorAll('.soundtrack'));
-// console.log(soundtrack);
 
 soundtrack.forEach((singleValue, index) => {
-    //console.log(singleValue.getAttribute('data-url'));
     let params = {
         container: '#soundtrack'+(index+1),
         waveColor: '#d7d7d7',
@@ -28,14 +19,11 @@ soundtrack.forEach((singleValue, index) => {
     let singlePlay = play[index];
 
     singlePlay.addEventListener('click', (e) => {
-        if(e.currentTarget) {
+        if(e.currentTarget === singlePlay) {
             playlists.playPause();
         } else {
             playlists.stop();
         }
-
-        console.log(singlePlay);
-        console.log(e.currentTarget);
 
         playlists.on('finish', () => {
             singlePlay.classList.remove('playing');
@@ -49,44 +37,3 @@ soundtrack.forEach((singleValue, index) => {
         }
     });
 });
-
-// for(let i = 0; i < playList.length; i++){
-//     let params = {
-//         container: '#soundtrack'+(i+1),
-//         waveColor: '#d7d7d7',
-//         progressColor: '#52d9a0',
-//         barWidth: 2,
-//         height: 44,
-//         cursorColor: '#d5d5d5',
-//         responsive: true
-//     }
-
-//     let playlists = WaveSurfer.create(params);
-//     playlists.load(playList[i]);
-
-//     const play = Array.from(document.querySelectorAll('.play-btn'));
-
-//     let singlePlay = play[i];
-
-//     singlePlay.addEventListener('click', (e) => {
-//         if(e.currentTarget) {
-//             playlists.playPause();
-//         } else {
-//             playlists.stop();
-//         }
-
-//         console.log(singlePlay);
-//         console.log(e.currentTarget);
-
-//         playlists.on('finish', () => {
-//             singlePlay.classList.remove('playing');
-//             playlists.stop();
-//         });
-
-//         if(playlists.isPlaying()) {
-//             singlePlay.classList.add('playing');
-//         } else {
-//             singlePlay.classList.remove('playing');
-//         }
-//     });
-// }
