7645937ab39e45e5fa8d1c56b1eede1c8fe46ada
Istiak-Tushar42 <istiak.tushar.qtec@gmail.com>
2021-09-25 01:46:27 +0600

    Final completed Puresounds

--- assets/js/scroll.js
+++ assets/js/scroll.js
@@ -5,16 +5,23 @@ let wrapper = document.querySelector('.wrapper');
 window.addEventListener('scroll', () => {
     let windowPosition = window.scrollY > 0;
 
+    const whiteLogo = document.querySelector('.whitelogo');
+    const blackLogo = document.querySelector('.blacklogo');
+
     if(windowPosition) {
         wrapper.classList.remove('dark-header-wrapper');
         header.classList.remove('dark-header');
         wrapper.classList.add('header-wrapper');
         header.classList.add('home-header');
+        blackLogo.classList.remove('d-none');
+        whiteLogo.classList.add('d-none');
     } else {
         wrapper.classList.remove('header-wrapper');
         header.classList.remove('home-header');
         wrapper.classList.add('dark-header-wrapper');
         header.classList.add('dark-header');
+        blackLogo.classList.add('d-none');
+        whiteLogo.classList.remove('d-none');
     }
 });
 
